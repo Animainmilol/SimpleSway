ab6491ac9e915831d67997f45ae12797d9325e34
Animain <animainmilol@gmail.com>
2024-09-07 09:33:14 +0500

    Starter

---
+++ sway/Scripts/record.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if pgrep -x "wf-recorder" > /dev/null; then
+    notify-send "Recording stopped!" "Saved in Videos folder"
+    pkill wf-recorder
+else
+    notify-send "Recording started!" "Press the shortcut again to stop the recording"
+    wf-recorder -f "$(xdg-user-dir VIDEOS)/$(date +'video_%Y-%m-%d-%H%M%S.mkv')"
+fi
+
